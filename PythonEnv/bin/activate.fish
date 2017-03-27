12701e6fc34754505b1186d2a27e78a67afa047e
olivier-dikken <olivierdikken@hotmail.fr>
2017-03-27 12:27:13 +0200

    pip install modules into virtualenv

--- PythonEnv/bin/activate.fish
+++ PythonEnv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dvpeer/Documents/Feedbackfruits/Challenge/TweetMatch/PythonEnv"
+set -gx VIRTUAL_ENV "/home/olivierdikken/Documents/BEPtest/TweetMatch/PythonEnv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
