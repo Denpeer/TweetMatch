a9bc8f4ffbf0c9446fe10bc4b68bfb1255a26324
Denpeer <D.J.P.vanPeer@student.tudelft.nl>
2017-03-27 15:31:08 +0200

    Bootstrap integration into flask. Modified version of default theme added.

--- WebServer/App/__init__.py
+++ WebServer/App/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config.from_object('config')
 
 from App import views # putting the import at the end avoids a circular import error
--- WebServer/App/forms.py
+++ WebServer/App/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
 
 class LoginForm(Form):
     tweet = StringField('tweet', validators=[DataRequired()])
+
+class TweetForm(Form):
+    tweet = StringField('tweet', widget=TextArea(),validators=[DataRequired()])
