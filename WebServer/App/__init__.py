6e84fb9ed61dca4bb1939c835cce5bb618cb262b
Denpeer <D.J.P.vanPeer@student.tudelft.nl>
2017-03-28 21:14:18 +0200

    Revert "Feature/multi feature"

--- WebServer/App/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-from App import views # putting the import at the end avoids a circular import error
--- WebServer/run.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-from App import app
-app.run(debug=True)
-
